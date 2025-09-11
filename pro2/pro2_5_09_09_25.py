7d0048534bac06ca7745b4b15aaf1a290819443b
Fabisex1707 <camikaz0102@gmail.com>
2025-09-11 02:36:13 -0600

    Uso de etiquetas para los  inidces de una serie

--- pro2/pro2_5_09_09_25.py
+++ pro2/pro2_5_09_09_25.py
@@ -1,11 +1,17 @@
 #uso de padas y la cotrcccio de series
 #Una serie es un array, un array es un conjuto de datos del mismo tipo, se diferecia de la lista en que la lista acepta diferentes tipos de datos 
 import pandas as pd
-
+#datos para la serie
 data_lista=[10,20,30]
+#creacion de la serie
 data_serie=pd.Series(data_lista)
-index_personalizado=["Fabi","Marta","Carlos"]
+#acceso a un dato de la serie por su indice
 print(data_serie[0])
+#Tipo de dato de un solo dato en la serie
 print(type(data_serie[0]))
+#creacion de la variable para guardar las etiquetas de los indices
+index_personalizado=["Fabi","Marta","Carlos"]
+#asigancion de las etiquetas a los indices
 data_serie.index=index_personalizado
+#vista con las etiquetas
 print(data_serie)
