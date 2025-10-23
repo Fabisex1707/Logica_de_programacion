843d264c1e0625ecceddd06dd2ec67ad2ca8715c
Fabisex1707 <camikaz0102@gmail.com>
2025-10-22 19:07:08 -0600

    Agrego nuevo demo de la ev3

--- Estructura_de_datos/pro2_8_Demo2EV.py
+++ Estructura_de_datos/pro2_8_Demo2EV.py
@@ -1,5 +1,8 @@
 from tabulate import tabulate
 import datetime
+import sqlite3
+from sqlite3 import Error
+import sys
 
 def convertir_str_a_date():
     while True:
@@ -247,13 +250,58 @@ def editar_nombre_reservacion(reservaciones: dict):
             print("Evento actualizado.")
             return
         print("El nombre no puede estar vacío.")
-    
 
+try:
+    with sqlite3.connect("reservaciones.db") as conn:
+        cursor = conn.cursor()
 
+        #activar llave foraneas
+        cursor.execute("PRAGMA foreign_keys = ON;")
 
+        #creacion de tabla clientes
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Clientes_registrados (
+                id_Cliente INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre_cliente TEXT NOT NULL,
+                apellidos_cliente TEXT NOT NULL
+            )
+        """)
 
-    
-        
+        #creacion de tabla salones
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Salones_registrados (
+                id_Salon INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre_salon TEXT NOT NULL,
+                cupo INTEGER NOT NULL,
+                turno_matutino TEXT NOT NULL,
+                turno_vespertino TEXT NOT NULL,
+                turno_nocturno TEXT NOT NULL
+            )
+        """)
+
+        #creacion de tabla reservaciones
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS reservaciones (
+                id_reservacion INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_cliente INTEGER NOT NULL,
+                nombre_cliente TEXT NOT NULL,
+                nombre_evento TEXT NOT NULL,
+                id_sala INTEGER NOT NULL,
+                nombre_salon TEXT NOT NULL,
+                fecha_reservacion TEXT NOT NULL,
+                turno TEXT NOT NULL,
+                FOREING KEY (id_cliente) REEFRENCES Clientes_registrados (id_Cliente) ON DELETE CASCADE,
+                FOREING KEY (id_sala) REEFRENCES Salones_registrados (id_Salon) ON DELETE CASCADE,
+                UNIQUE (id_sala, fecha_reservacion, turno)
+            )
+        """)
+except Error as e:
+    print(f"Error al conectar con la base de datos: {e}")
+except:
+    print(f"Ocurrio un error inesperado de tipo: {sys.exc_info()[0]}")
+finally:
+    conn.close()
+
 
 def main():
     id_reservaciones_contador = 2
